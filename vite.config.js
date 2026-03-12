fc3df7993dbae41d5e8df47d30218020ef149746
ankushgupta05 <ankushguptagupta725@gmail.com>
2026-03-13 02:13:49 +0530

    first

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,3 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import tailwindcss from '@tailwindcss/vite'
-
-// export default defineConfig({
-//   plugins: [
-//     react(),
-//     tailwindcss(),
-//   ],
-// })
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
@@ -21,10 +10,10 @@ export default defineConfig({
   build: {
     chunkSizeWarningLimit: 1000,
     assetsInlineLimit: 0,
+    minify: 'esbuild',
     rollupOptions: {
       output: {
         manualChunks: {
-          vendor: ['react', 'react-dom'],
           mui: ['@mui/material', '@mui/icons-material'],
           redux: ['@reduxjs/toolkit', 'react-redux'],
         }
